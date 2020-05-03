0620b4dde9c1a275ceb97d77847de106996ee24a
IusanCatalin <62466618+IusanCatalin@users.noreply.github.com>
2020-05-03 20:43:04 +0300

    updated LocationService

--- automotive/src/main/java/com/example/roadmission/LocationService.kt
+++ automotive/src/main/java/com/example/roadmission/LocationService.kt
@@ -1,26 +1,20 @@
 package com.example.roadmission
-import android.app.Activity
+
 import android.app.Service
-import android.content.ComponentName
-import android.content.Context
 import android.content.Intent
-import android.content.ServiceConnection
 import android.location.Location
 import com.google.android.gms.location.LocationListener;
 import android.os.Binder
 import android.os.Bundle
 import android.os.IBinder
-import android.widget.Chronometer
-import android.widget.Toast
+import android.os.SystemClock
 import com.google.android.gms.common.ConnectionResult
 import com.google.android.gms.common.api.GoogleApiClient
 import com.google.android.gms.location.LocationRequest
 import com.google.android.gms.location.LocationServices
-import kotlinx.android.synthetic.main.fragment_right.view.*
 
 class LocationService : Service(), GoogleApiClient.ConnectionCallbacks,GoogleApiClient.OnConnectionFailedListener,
     LocationListener{
-
 
     private val INTERVAL: Long = 1000 * 2
     private val FASTESTINTERVAL: Long = 1000
@@ -32,24 +26,16 @@ class LocationService : Service(), GoogleApiClient.ConnectionCallbacks,GoogleApi
     private var currentLocation: Location = Location("dummylocation")
     private var mBinder:IBinder = LocalBinder()
     private var minutesStopped:Int = 0
+    private var distance:Double = 0.0
 
     init {
-        checkForStop()
+        android.os.Handler().postDelayed(Runnable {updateStop()}, 60000) //1minute - 60.000
     }
 
-    private fun checkForStop(){
-        if(lastLocation == currentLocation){
-            minutesStopped += 1
-            RightFragment.chronometer.start()
-        }
-        else if(minutesStopped >= 5){  //minim 5 minute pauza
-            minutesStopped = 0
-        }
-        else
-        {
-            minutesStopped = 0
-        }
-        android.os.Handler().postDelayed(Runnable {checkForStop()}, 60000) //1minute
+    private fun updateStop(){
+        RightFragment.chronometer.start()
+        minutesStopped += 1
+        android.os.Handler().postDelayed(Runnable {updateStop()}, 60000) //1minute
     }
 
     override fun onBind(intent: Intent?): IBinder? {
@@ -61,16 +47,12 @@ class LocationService : Service(), GoogleApiClient.ConnectionCallbacks,GoogleApi
             .build()
         mGoogleApiClient.connect()
         return mBinder
-
     }
 
     inner class LocalBinder : Binder() {
-
         fun getService (): LocationService {
             return this@LocationService
         }
-
-
     }
 
     fun createLocationRequest(){
@@ -78,7 +60,6 @@ class LocationService : Service(), GoogleApiClient.ConnectionCallbacks,GoogleApi
         mlocationRequest.setInterval(INTERVAL)
         mlocationRequest.setFastestInterval(FASTESTINTERVAL)
         mlocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
-
     }
 
     override
@@ -86,10 +67,7 @@ class LocationService : Service(), GoogleApiClient.ConnectionCallbacks,GoogleApi
     {
         try{
             LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient,mlocationRequest,this)
-        }catch (e:SecurityException)
-        {
-
-        }
+        }catch (e:SecurityException) { }
 
     }
     override
@@ -105,8 +83,16 @@ class LocationService : Service(), GoogleApiClient.ConnectionCallbacks,GoogleApi
 
     override fun onLocationChanged(location: Location) {
         lastLocation = currentLocation
-        currentLocation= location
-
+        currentLocation = location
+        distance = lastLocation.distanceTo(currentLocation)/1.0
+        if(distance > 1.00) {
+            if(minutesStopped >= 5)
+            {
+                RightFragment.chronometer.setBase(SystemClock.elapsedRealtime());
+                RightFragment.chronometer.stop();
+            }
+            minutesStopped = 0
+        }
     }
 
     override fun onUnbind(intent:Intent):Boolean{
